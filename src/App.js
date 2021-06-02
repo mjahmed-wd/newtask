03d2c758df8c21201f3bddadecc985345d714059
shantovai23 <mjahemd.wd@gmail.com>
2021-06-02 15:11:10 +0600

    with quantity

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 // import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Select from "react-select";
 
 function App() {
-  
-
   const productOptions = [
     { value: "Potato", label: "Potato" },
     { value: "Mojo", label: "Mojo" },
@@ -14,11 +12,27 @@ function App() {
   ];
 
   const [rowDto, setRowDto] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [totalDiscount, setTotalDiscount] = useState(0);
+
+  useEffect(() => {
+    const totalAmount = rowDto?.reduce(
+      (acc, item) => acc + +item?.netAmount,
+      0
+    );
+    const totalDiscount = rowDto?.reduce(
+      (acc, item) => acc + +item?.discount,
+      0
+    );
+    setTotalAmount(totalAmount);
+    setTotalDiscount(totalDiscount);
+  }, [rowDto]);
 
   const addHandler = (v) => {
     const obj = {
       ...v,
       price: "",
+      quantity: 1,
       discount: "",
       netAmount: "",
     };
@@ -28,7 +42,35 @@ function App() {
   const rowDtoChangeHandler = (name, value, i) => {
     const data = [...rowDto];
     data[i][name] = value;
-    setRowDto(data)
+    setRowDto(data);
+  };
+
+  // const reducer = (name, setter) => {
+  //   const netAmountArray = rowDto.map((product) =>
+  //     product[name] ? product[name] : 0
+  //   );
+  //   setter(
+  //     netAmountArray.reduce(
+  //       (accumulator, currentValue) =>
+  //         parseInt(accumulator) + parseInt(currentValue)
+  //     )
+  //   );
+  // };
+
+  const calculationHandler = (e, i, item) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    rowDtoChangeHandler(name, value, i);
+    const netAmount =
+      name === "price"
+        ? value - item?.discount
+        : name === "discount"
+        ? item?.price * item?.quantity - value
+        : item?.price * value;
+    rowDtoChangeHandler("netAmount", netAmount, i);
+
+    // reducer("netAmount", setTotalAmount);
+    // reducer("discount", setTotalDiscount);
   };
 
   return (
@@ -44,35 +86,43 @@ function App() {
       {/* Table  */}
       <table className="table">
         <thead>
-          <th>Item</th>
-          <th>Price</th>
-          <th>Discount</th>
-          <th>Net Amount</th>
+          <tr>
+            <th>Item</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Discount</th>
+            <th>Net Amount</th>
+          </tr>
         </thead>
         <tbody>
           {rowDto?.map((item, i) => (
-            <tr>
+            <tr key={i}>
               <td>{item?.label}</td>
               <td>
                 <input
                   name="price"
+                  type="number"
+                  min={0}
                   value={item?.price}
-                  onChange={(e) => {
-                    rowDtoChangeHandler(e.target.name, e.target.value, i);
-                    const netAmount = e.target.value - item?.discount || 0;
-                    rowDtoChangeHandler("netAmount", netAmount, i);
-                  }}
+                  onChange={(e) => calculationHandler(e, i, item)}
+                />
+              </td>
+              <td>
+                <input
+                  name="quantity"
+                  type="number"
+                  min={0}
+                  value={item?.quantity}
+                  onChange={(e) => calculationHandler(e, i, item)}
                 />
               </td>
               <td>
                 <input
                   name="discount"
+                  type="number"
+                  min={0}
                   value={item?.discount}
-                  onChange={(e) => {
-                    rowDtoChangeHandler(e.target.name, e.target.value, i);
-                    const netAmount = item?.price - e.target.value;
-                    rowDtoChangeHandler("netAmount", netAmount, i);
-                  }}
+                  onChange={(e) => calculationHandler(e, i, item)}
                 />
               </td>
               <td>{item?.netAmount}</td>
@@ -80,6 +130,10 @@ function App() {
           ))}
         </tbody>
       </table>
+      <p>Total Amount</p>
+      <p>{totalAmount}</p>
+      <p>Total Discount</p>
+      <p>{totalDiscount}</p>
     </div>
   );
 }
